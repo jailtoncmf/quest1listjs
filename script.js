7fcadc40a8d6b18b16114905040e8e712af11a96
jailtoncmf <102435314+jailtoncmf@users.noreply.github.com>
2023-09-10 23:33:12 +0000

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 5;
+var c = 1;
+var d;
+
+b--;
+
+document.write("c = " + b + "<br>");
+
+a++;
+
+document.write("b = ", a, "<br>");
+
+a = 5 + "1";
+document.write("a = " + a + "<br>");
+
+d = "1" + "1";
+document.write("d = " + d + "<br>");
